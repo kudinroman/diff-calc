92192ad2927feabe9bdc2768bcbd0066e1288c31
roman <kudinroman82@gmail.com>
2018-04-03 14:51:52 +0300

    add commander.js, implement help output

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
-console.log('Welcome to the GenDiff');
+
+import commander from 'commander';
+
+commander
+  .command('setup <firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
