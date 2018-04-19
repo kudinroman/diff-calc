491ecb3edaad52bec6f3ac2cf16857cbabba0559
roman <kudinroman82@gmail.com>
2018-04-19 20:00:22 +0300

    edit .action (add log)

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
-import gendiff from '..';
+import genDiff from '..';
 
 commander
   .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => gendiff(firstConfig, secondConfig))
+  .action((firstConfig, secondConfig) => {
+    const diff = genDiff(firstConfig, secondConfig);
+    console.log(diff);
+  })
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
 
-export default gendiff;
+export default genDiff;
