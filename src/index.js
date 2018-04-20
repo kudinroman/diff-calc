4c11b7cabfa6ae2759c7d704ee277b2ab164f61f
roman <kudinroman82@gmail.com>
2018-04-20 15:30:27 +0300

    edit func gendiff in the index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import _ from 'lodash';
 import fs from 'fs';
 
-const gendiff = (pathToFile1, pathToFile2) => {
+const genDiff = (pathToFile1, pathToFile2) => {
   const dataBeforeChange = JSON.parse(fs.readFileSync(pathToFile1));
   const dataAfterChange = JSON.parse(fs.readFileSync(pathToFile2));
-  const commonKeysArr = Object.keys({ ...dataBeforeChange, ...dataAfterChange });
+  const Keys = _.union(Object.keys(dataBeforeChange), Object.keys(dataAfterChange));
 
-  const result = commonKeysArr.reduce((acc, key) => {
-    const isDeleted = !_.has(dataAfterChange, key) && _.has(dataBeforeChange, key);
-    const isAdded = !_.has(dataBeforeChange, key) && _.has(dataAfterChange, key);
+  const result = Keys.reduce((acc, key) => {
+    const isDeleted = !_.has(dataAfterChange, key);
+    const isAdded = !_.has(dataBeforeChange, key);
     const isChanged = _.has(dataAfterChange, key) && _.has(dataBeforeChange, key)
       && dataAfterChange[key] !== dataBeforeChange[key];
-    const isUnchanged = _.has(dataAfterChange, key) && _.has(dataBeforeChange, key)
-      && dataAfterChange[key] === dataBeforeChange[key];
+    const isUnchanged = dataAfterChange[key] === dataBeforeChange[key];
 
     if (isDeleted) {
       return `${acc}\n  - ${key}: ${dataBeforeChange[key]}`;
@@ -29,4 +28,4 @@ const gendiff = (pathToFile1, pathToFile2) => {
   return `{${result}\n}\n`;
 };
 
-export default gendiff;
+export default genDiff;
