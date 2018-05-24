c3547569fe921c3546fdda96f74c350957bf5ae1
roman <kudinroman82@gmail.com>
2018-05-24 19:28:17 +0300

    improve the previous implementation

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,26 +3,26 @@ import gendiff from '../src/bin/gendiff.js';
 
 const beforeJson = '__tests__/__fixtures__/json/before.json';
 const afterJson = '__tests__/__fixtures__/json/after.json';
-const resultJson = fs.readFileSync(`__tests__/__fixtures__/json/resultJson.txt`, 'utf8');
+const resultJson = `__tests__/__fixtures__/json/resultJson.txt`;
 
 const beforeYaml = '__tests__/__fixtures__/yaml/before.yaml';
 const afterYaml = '__tests__/__fixtures__/yaml/after.yaml';
-const resultYaml = fs.readFileSync(`__tests__/__fixtures__/yaml/resultYaml.txt`, 'utf8');
+const resultYaml = `__tests__/__fixtures__/yaml/resultYaml.txt`;
 
 const beforeIni = '__tests__/__fixtures__/ini/before.ini';
 const afterIni = '__tests__/__fixtures__/ini/after.ini';
-const resultIni = fs.readFileSync(`__tests__/__fixtures__/ini/resultIni.txt`, 'utf8');
+const resultIni = `__tests__/__fixtures__/ini/resultIni.txt`;
 
 describe('compare two files', () => {
   test('compare two files .json', () => {
-    expect(gendiff(beforeJson, afterJson)).toBe(resultJson);
+    expect(gendiff(beforeJson, afterJson)).toBe(fs.readFileSync(resultJson, 'utf8'));
   });
 
   test('compare two files .yaml', () => {
-    expect(gendiff(beforeYaml, afterYaml)).toBe(resultYaml);
+    expect(gendiff(beforeYaml, afterYaml)).toBe(fs.readFileSync(resultYaml, 'utf8'));
   });
 
   test('compare two files .ini', () => {
-    expect(gendiff(beforeIni, afterIni)).toBe(resultIni);
+    expect(gendiff(beforeIni, afterIni)).toBe(fs.readFileSync(resultIni, 'utf8'));
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 import path from 'path';
-import gendiffJson from './actions/gendiffJson';
-import gendiffYaml from './actions/gendiffYaml';
-import gendiffIni from './actions/gendiffIni';
+import _ from 'lodash';
+import fs from 'fs';
+import yaml from 'js-yaml';
+import ini from 'ini';
 
-const gendiffs = [
-  {
-    name: '.json',
-    func: gendiffJson,
-  },
-  {
-    name: '.yaml',
-    func: gendiffYaml,
-  },
-  {
-    name: '.ini',
-    func: gendiffIni,
-  },
-];
-const gendiff = (pathToFile1, pathToFile2) =>
-  gendiffs.slice().filter(elem => elem.name === path.extname(pathToFile1))[0]
-    .func(pathToFile1, pathToFile2);
+const gendiffs = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.ini': ini.parse,
+};
+const getFuncGenDiff = pathToFile => gendiffs[path.extname(pathToFile)];
+
+const gendiff = (pathToFile1, pathToFile2) => {
+  const dataBeforeChange = getFuncGenDiff(pathToFile1)(fs.readFileSync(pathToFile1).toString());
+  const dataAfterChange = getFuncGenDiff(pathToFile1)(fs.readFileSync(pathToFile2).toString());
+  const Keys = _.union(Object.keys(dataBeforeChange), Object.keys(dataAfterChange));
+
+  const result = Keys.reduce((acc, key) => {
+    const isDeleted = !_.has(dataAfterChange, key);
+    const isAdded = !_.has(dataBeforeChange, key);
+    const isChanged = _.has(dataAfterChange, key) && _.has(dataBeforeChange, key)
+      && dataAfterChange[key] !== dataBeforeChange[key];
+    const isUnchanged = dataAfterChange[key] === dataBeforeChange[key];
+
+    if (isDeleted) {
+      return `${acc}\n  - ${key}: ${dataBeforeChange[key]}`;
+    } else if (isAdded) {
+      return `${acc}\n  + ${key}: ${dataAfterChange[key]}`;
+    } else if (isUnchanged) {
+      return `${acc}\n    ${key}: ${dataBeforeChange[key]}`;
+    } else if (isChanged) {
+      return `${acc}\n  + ${key}: ${dataAfterChange[key]}\n  - ${key}: ${dataBeforeChange[key]}`;
+    }
+    return `${acc}`;
+  }, '');
+
+  return `{${result}\n}\n`;
+};
 
 export default gendiff;
